4eafc7fef4db31ebf9c0a78dd81d244570f156b2
victor-carvalho1996 <victorpcarvalho18@gmail.com>
2021-01-31 19:07:54 -0300

    Delete Seller

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -7,7 +7,7 @@ namespace SalesWebMvc.Models
 {
     public class Seller
     {
-        public int id { get; set; }
+        public int Id { get;  set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
@@ -15,6 +15,7 @@ namespace SalesWebMvc.Models
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+        
 
         public Seller()
         {
@@ -23,7 +24,7 @@ namespace SalesWebMvc.Models
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
-            this.id = id;
+            this.Id = id;
             Name = name;
             Email = email;
             BirthDate = birthDate;
